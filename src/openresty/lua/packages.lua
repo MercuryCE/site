bc78bdfd8de5822c3613be447318e18c94823b23
Sledmine <40512223+Sledmine@users.noreply.github.com>
2021-11-05 19:12:39 -0700

    update packages index

--- src/openresty/lua/packages.lua
+++ src/openresty/lua/packages.lua
@@ -1,7 +1,7 @@
 local json = require "cjson"
 local template = require "resty.template"
 
-local jsonFile = io.open(ngx.config.prefix() .. "packagesList.json", "r")
+local jsonFile = io.open(ngx.config.prefix() .. "packages.json", "r")
 local jsonContent = jsonFile:read("*all")
 
 local packagesList = json.decode(jsonContent)
