96b496a835e3d9f610ab75c2fbeb8f5d45e064c3
Sledmine <40512223+Sledmine@users.noreply.github.com>
2021-12-01 02:27:14 -0700

    attempt to retrieve packages from api

--- src/openresty/lua/packages.lua
+++ src/openresty/lua/packages.lua
@@ -1,15 +1,21 @@
 local json = require "cjson"
 local template = require "resty.template"
-
-local jsonFile = io.open(ngx.config.prefix() .. "packages.json", "r")
-local jsonContent = jsonFile:read("*all")
-
-local packagesList = json.decode(jsonContent)
+local requests = require "resty.requests"
 
 local routesFile = io.open(ngx.config.prefix() .. "routes.json", "r")
 local routesContent = routesFile:read("*all")
 local routesTable = json.decode(routesContent)
 
-template.render("pages/packages.html", {
-    packages = packagesList, routes = routesTable
-})
+local r, err = requests.get("http://vulcano.shadowmods.net/fetch")
+if not r then
+    ngx.log(ngx.ERR, err)
+    local jsonFile = io.open(ngx.config.prefix() .. "packagesList.json", "r")
+    local jsonContent = jsonFile:read("*all")
+
+    local packagesList = json.decode(jsonContent)
+
+    template.render("pages/packages.html", {packages = packagesList, routes = routesTable})
+    return
+end
+local packagesList = json.decode(r)
+template.render("pages/packages.html", {packages = packagesList, routes = routesTable})
