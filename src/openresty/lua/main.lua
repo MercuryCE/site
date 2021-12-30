c1982639ed3a03675a582d961d8b4d7f432568f2
Sledmine <40512223+Sledmine@users.noreply.github.com>
2021-12-30 01:37:59 -0700

    minor fixes, add manual zip download for packages

--- src/openresty/lua/main.lua
+++ src/openresty/lua/main.lua
@@ -1,6 +1,12 @@
 local json = require "cjson"
 LatestPackages = {}
 
+function TableLength(T)
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
+end
+
 ngx.log(ngx.DEBUG, "Fetching latest packages...")
 local pipe = io.popen("curl -q http://vulcano.shadowmods.net/v1/fetch")
 if (pipe) then
